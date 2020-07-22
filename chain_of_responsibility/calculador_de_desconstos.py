d83b660718dbb8f339eadf4494aaae0e26021b20
Emmanuel de Oliveira D'Abruzzo <emmanuel.oliveira@trtsp.jus.br>
2020-07-22 05:44:39 -0300

    Começa a separar as responsabilidades em classes através do padrão chainOfResponibility

--- chain_of_responsibility/calculador_de_desconstos.py
+++ chain_of_responsibility/calculador_de_desconstos.py
@@ -5,19 +5,30 @@ class CalculadorDescontos(object):
 
     def calculaDesconto(self, orcamento):
 
+        desconto = 0
+
         if orcamento.quantidade_itens() > 5:
-            desconto = orcamento.valor * 0.10;
+            desconto = orcamento.valor * 0.10
 
         elif orcamento.valor > 500.00:
-            desconto = orcamento.valor * 0.07;
+            desconto = orcamento.valor * 0.07
 
-        print('Valor do desconto: '+desconto)
+        print('Valor do desconto: ' + str(desconto))
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
 
-    item = Item(nome='X', valor=501)
-    orcamento = Orcamento()
-    orcamento.adicionaItem(item)
+    orcamentoA = Orcamento()
+    orcamentoA.adicionaItem(Item(nome='X', valor=501))
+    orcamentoB = Orcamento()
+    orcamentoB.adicionaItem(Item(nome='X', valor=400))
+    orcamentoC = Orcamento()
+    orcamentoC.adicionaItem(Item('Item A', 100.0))
+    orcamentoC.adicionaItem(Item('Item B', 50.0))
+    orcamentoC.adicionaItem(Item('Item C', 400.0))
+    orcamentoC.adicionaItem(Item('Item D', 100.0))
+    orcamentoC.adicionaItem(Item('Item E', 100.0))
 
-    CalculadorDescontos().calculaDesconto(orcamento)    
+    CalculadorDescontos().calculaDesconto(orcamentoA)
+    CalculadorDescontos().calculaDesconto(orcamentoB)
+    CalculadorDescontos().calculaDesconto(orcamentoC)
