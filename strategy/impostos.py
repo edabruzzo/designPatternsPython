c84b4e8311e57e54bc296873e572827c7fde0371
Emmanuel de Oliveira D'Abruzzo <emmanuel.oliveira@trtsp.jus.br>
2020-07-22 09:09:28 -0300

    Adiciona outros impostos

--- strategy/impostos.py
+++ strategy/impostos.py
@@ -11,3 +11,32 @@ class ICMS(object):
     #https://stackoverflow.com/questions/23944657/typeerror-method-takes-1-positional-argument-but-2-were-given
     def calcular(self, orcamento):
         return orcamento.valor * 0.06
+
+
+
+class ICPP(object):
+
+    def calcula(self, orcamento):
+        if orcamento.valor > 500:
+            return orcamento.valor * 0.07
+        else:
+            return orcamento.valor * 0.05
+
+
+class IKCV(object):
+
+    def calcula(self, orcamento):
+        if orcamento.valor > 500 and self.__tem_item_maior_que_100_reais(orcamento):
+            return orcamento.valor * 0.10
+        else:
+            return orcamento.valor * 0.06
+
+    def __tem_item_maior_que_100_reais(self, orcamento):
+        for item in orcamento.obter_itens():
+            if item.valor > 100:
+                return True
+        return False
+
+
+
+
